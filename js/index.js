20e6ae578a19361bb7c89e25eebcfe528a8d5fa5
Raju Tamang <rajutamang3456@gmail.com>
2026-03-14 15:49:01 +0545

    changes

--- js/index.js
+++ js/index.js
@@ -1,2 +1,2 @@
-// const  conf= confirm("are you fullstack developer");
-// console.log("this is my confirm value", conf);
+const  conf= confirm("are you fullstack developer");
+// console.log("this is my confirm value", conf);// 
